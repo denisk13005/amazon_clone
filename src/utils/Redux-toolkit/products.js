1fc5200ae4ef80801ff662bea0ef450fee0398d1
kirmann <k.dkle@hotmail.fr>
2022-05-27 09:16:12 +0200

    début de calcul du total

--- src/utils/Redux-toolkit/products.js
+++ src/utils/Redux-toolkit/products.js
@@ -7,19 +7,25 @@ const productsSlice = createSlice({
     basketItems: 0,
     loading: false,
     error: null,
+    totalPrice: 0,
   },
   reducers: {
     addProduct: (state, action) => {
       console.log(action.payload)
       state.products = [...state.products, action.payload]
+      //on incrémente le nb d'items dans le panier
       state.basketItems++
+      //on met à jour le total
+      state.totalPrice = state.totalPrice + action.payload.price
     },
     removeProduct: (state, action) => {
       //on retourne un tableau qui comprend tous les autres éléments sauf celui avec l'id cliqué
       state.products = state.products.filter(
         (product) => product.id !== action.payload
       )
+      //on dévrémente le nb d'items dans le panier
       state.basketItems--
+      console.log("remove", state.totalPrice - action.payload.price)
     },
   },
 })
