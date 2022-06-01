1ed8af550fa5bf629f77bcb4e5bc23b3fa885460
kirmann <k.dkle@hotmail.fr>
2022-06-01 16:54:51 +0200

    début page payment

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "./payment.scss"
+import logo from "../../assets/img/blackLogo.png"
 
 const Payment = () => {
   return (
@@ -7,10 +8,17 @@ const Payment = () => {
       className="paymentContainer
   "
     >
-      <div className="paymentTitle">
-        <h1>Payment</h1>
+      <div className="headerPayment">
+        <div className="headerContent">
+          <img className="paymentLogo" src={logo} alt="amazon logo" />
+          <h1>Passer la commande </h1>
+          <img
+            className="paymentPadlock"
+            src="https://images-eu.ssl-images-amazon.com/images/G/08/x-locale/checkout/truespc/secured-ssl._CB485936936_.png"
+            alt=""
+          />
+        </div>
       </div>
-      Payment
     </section>
   )
 }
